40da3d68efefd058ddc8727e13a4a36a825b839d
Javier Cardona <javier@cardonabits.com>
2023-05-21 13:45:21 +0100

    Convert images to 1bpp

--- src/App.js
+++ src/App.js
@@ -11,22 +11,38 @@ import 'jimp'
 
 import './App.css';
 
+var bmpJs = require('bmp-js');
+var bit1Encoder = require('./encoder');
+
 const ScaledImage = props => {
   const { Jimp } = window;
   const [imageData, setImageData] = React.useState("");
   React.useEffect(() => {
     const processImage = (file) => {
       let file_url = URL.createObjectURL(file);
+      // First we read the image with Jimp, do initial processing
+      // and convert to 1bpp.  The 1bpp image is then read again
+      // with Jimp so we can convert to Base64 to be used
+      // by the 'img' element.
       Jimp.read(file_url)
         .then((f) => {
           f
             .resize(256, 256) // resize
-            .quality(60) // set JPEG quality
-            .greyscale() // set greyscale
-            .getBase64Async(Jimp.MIME_JPEG)
-            .then(buffer => {
-              setImageData(buffer);
-              console.log('buffer len: ' + buffer.length)
+            .background(0xFFFFFFFF)
+            .flip(false, true)
+            .getBuffer(Jimp.MIME_BMP, (err, data) => {
+              if (err) throw err;
+              var bmpData = bmpJs.decode(data);
+              const bit1bmp = bit1Encoder(bmpData, 1);
+              // fs.writeFileSync(path.join(dir, `${name}.bmp`), bit1.data);
+              console.log('bit1bmp: ' + bit1bmp)
+              Jimp.read(bit1bmp.data)
+              .then((f) => {
+                f.getBase64Async(Jimp.MIME_BMP)
+                .then(buffer => {
+                  setImageData(buffer);
+                })
+              })
             });
         })
         .catch((err) => {
