67cea3f77de891310bed9b91eba8e030fec0f1a5
Javier Cardona <javier@cardonabits.com>
2023-06-01 12:23:52 +0100

    Started working on ROM generation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {
   // fileOpen,
   directoryOpen,
-  // fileSave,
-  // supported,
+  fileSave,
+  supported,
 } from 'browser-fs-access';
 // import Jimp from 'jimp';
 // See https://github.com/jimp-dev/jimp/issues/1194
@@ -18,58 +18,96 @@ const ScaledImage = props => {
   const { Jimp } = window;
   const [imageData, setImageData] = React.useState("");
   React.useEffect(() => {
-    const processImage = (file) => {
-      let file_url = URL.createObjectURL(file);
-      // First we read the image with Jimp, do initial processing
-      // and convert to 1bpp.  The 1bpp image is then read again
-      // with Jimp so we can convert to Base64 to be used
-      // by the 'img' element.
-      Jimp.read(file_url)
-        .then((f) => {
+    let file_url = URL.createObjectURL(props.file);
+    // First we read the image with Jimp, do initial processing
+    // and convert to 1bpp.  The 1bpp image is then read again
+    // with Jimp so we can convert to Base64 to be used
+    // by the 'img' element.
+    Jimp.read(file_url)
+      .then((f) => {
+        f
+          .resize(20, 20)
+          .background(0xFFFFFFFF)
+          .flip(false, true)
+          .getBuffer(Jimp.MIME_BMP, (err, data) => {
+            if (err) throw err;
+            var bmpData = bmpJs.decode(data);
+            const bit1bmp = bit1Encoder(bmpData, 1);
+            console.log('bit1bmp size: ' + bit1bmp.data.length)
+            Jimp.read(bit1bmp.data)
+              .then((f) => {
+                f.getBase64Async(Jimp.MIME_BMP)
+                  .then(buffer => {
+                    setImageData(buffer);
+                  })
+              })
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }, [props.file]);
+  return <img alt={props.file.name} src={imageData}></img>
+}
+
+export default function App() {
+  if (supported) {
+    console.log('Using the File System Access API.');
+  } else {
+    console.log('Using the fallback implementation.');
+  }
+  const [files, setFiles] = React.useState([]);
+
+  const openDirectory = async () => {
+    const blobsInDirectory = await directoryOpen({
+      recursive: true,
+    })
+    setFiles(blobsInDirectory.sort((a, b) => { return collator.compare(a.name, b.name) }));
+  };
+
+  const generateROM = async () => {
+    const { Jimp } = window;
+    let read_files = files.map((file, index) => {
+      return Jimp.read(URL.createObjectURL(file))
+        .then(f => {
           f
-            .resize(200, 200) // resize
+            .resize(20, 20)
             .background(0xFFFFFFFF)
             .flip(false, true)
             .getBuffer(Jimp.MIME_BMP, (err, data) => {
               if (err) throw err;
               var bmpData = bmpJs.decode(data);
               const bit1bmp = bit1Encoder(bmpData, 1);
-              // fs.writeFileSync(path.join(dir, `${name}.bmp`), bit1.data);
               console.log('bit1bmp size: ' + bit1bmp.data.length)
-              Jimp.read(bit1bmp.data)
-              .then((f) => {
-                f.getBase64Async(Jimp.MIME_BMP)
+              return Jimp.read(bit1bmp.data);
+            })
+            .then((f) => {
+              f.getBase64Async(Jimp.MIME_BMP)
                 .then(buffer => {
-                  setImageData(buffer);
+                  return buffer;
                 })
-              })
-            });
+            })
         })
         .catch((err) => {
           console.error(err);
         })
-    }
-    processImage(props.file);
-  }, [props.file]);
-  return <img alt={props.file.name} src={imageData}></img>
-}
+    });
 
-export default function App() {
-  const [ files, setFiles ] = React.useState([]);
-  const openDirectory = async () => {
-    const blobsInDirectory  = await directoryOpen({
-      recursive: true,
-    })
-    setFiles(blobsInDirectory.sort((a, b) => { return collator.compare(a.name, b.name)} ));
+    console.log(read_files);
+    // await fileSave(files[0], {
+    //   fileName: 'lightnote.rom',
+    //   extensions: ['.rom'],
+    // });
   };
 
   const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
   return (
     <div>
       <button onClick={() => openDirectory()}>Select Images Directory</button>
+      <button onClick={() => generateROM()}>Generate ROM</button>
       <br />
       {files.map((file, index) => (
-          <ScaledImage key={index} file={file} />
+        <ScaledImage key={index} file={file} />
       ))}
     </div>
   );
