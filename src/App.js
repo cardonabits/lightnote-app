d8e408db960044c04d34dc23dc2acff0c66f65db
Javier Cardona <javier@cardonabits.com>
2023-08-19 19:05:30 +0100

    Run npm audit --force

--- src/App.js
+++ src/App.js
@@ -10,101 +10,12 @@ import {
 // See https://github.com/jimp-dev/jimp/issues/1194
 import 'jimp'
 import './App.css';
+import { OriginalImage } from './OriginalImage';
+import { ScaledImage } from './ScaledImage';
 
-var floydSteinberg = require('floyd-steinberg');
 
 var bmpJs = require('bmp-js');
 var bit1Encoder = require('./encoder');
-
-// input is a rectangular image, output is the
-// image cropped to the largest square contained in
-// it
-function center_square(image) {
-  if (image.bitmap.width > image.bitmap.height) {
-    image.crop((image.bitmap.width - image.bitmap.height)/2, 0, image.bitmap.height, image.bitmap.height)
-  } else {
-    image.crop(0, (image.bitmap.height - image.bitmap.width)/2, image.bitmap.width, image.bitmap.width)
-  }
-}
-
-const ScaledImage = props => {
-  const { Jimp } = window;
-  const [imageData, setImageData] = React.useState("");
-  React.useEffect(() => {
-    if (props.file === "")
-      return;
-    let file_url = URL.createObjectURL(props.file);
-    // First we read the image with Jimp, do initial processing
-    // and convert to 1bpp.  The 1bpp image is then read again
-    // with Jimp so we can convert to Base64 to be used
-    // by the 'img' element.
-    Jimp.read(file_url)
-      .then((f) => {
-        // resize, grayscale and clone
-        f.resize(400, Jimp.AUTO)
-          .grayscale();
-
-        // crop square around center
-        center_square(f);
-
-        // resize
-        f.resize(200, 200)
-        props.setProgress(25);
-
-        // There is no jimp plugin for dithering, using a different
-        // package
-        f.bitmap = floydSteinberg(f.bitmap)
-        return f
-          .getBufferAsync(Jimp.MIME_BMP)
-      })
-      .then(data => {
-        var bmpData = bmpJs.decode(data);
-        const bit1bmp = bit1Encoder.bmp(bmpData, 1);
-        props.setProgress(50);
-        return Jimp.read(bit1bmp.data)
-      })
-      .then((f) => {
-        // The decode/encode process flips the image.  Revert.
-        f.flip(false, true)
-        return f
-          .getBase64Async(Jimp.MIME_BMP)
-      })
-      .then(buffer => {
-        props.setProgress(75);
-        setImageData(buffer);
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-      props.setProgress(100);
-  }, [props.file]);
-  return <div>
-        <img alt="" style={imageData !== "" ? {} : { display: 'none' }} src={imageData} />
-        </div>
-}
-
-const OriginalImage = props => {
-  const { Jimp } = window;
-  const [imageData, setImageData] = React.useState("");
-  React.useEffect(() => {
-    if (props.file === "")
-      return;
-    let file_url = URL.createObjectURL(props.file);
-    Jimp.read(file_url)
-      .then((f) => {
-        return f
-          .resize(400, Jimp.AUTO)
-          .getBase64Async(Jimp.MIME_JPEG)
-      })
-      .then(buffer => {
-        setImageData(buffer);
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  }, [props.file]);
-  return <img alt="" style={imageData !== "" ? {} : { display: 'none' }} src={imageData} />
-}
 
 export default function App() {
   if (supported) {
@@ -121,7 +32,6 @@ export default function App() {
       description: 'Image file',
     })
     setFile(imageBlob);
-    setProgress(100);
   };
 
   const generateROM = async () => {
