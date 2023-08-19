26047f5aad8f7bc0a0efbb5a1f57ae5261d0c397
Javier Cardona <javier@cardonabits.com>
2023-08-19 20:50:28 +0100

    Generate a BMP file from the scaled image

--- src/App.js
+++ src/App.js
@@ -14,9 +14,6 @@ import { OriginalImage } from './OriginalImage';
 import { ScaledImage } from './ScaledImage';
 
 
-var bmpJs = require('bmp-js');
-var bit1Encoder = require('./encoder');
-
 export default function App() {
   if (supported) {
     console.log('Using the File System Access API.');
@@ -25,7 +22,7 @@ export default function App() {
   }
   const [file, setFile] = React.useState("");
   const [progress, setProgress] = React.useState(0);
-  const [scaledImageBuffer, setScaledImageBuffer] = React.useState();
+  const [imageBuffer, setImageBuffer] = React.useState(null);
 
   const openFile = async () => {
     const imageBlob = await fileOpen({
@@ -36,59 +33,17 @@ export default function App() {
   };
 
   const generateROM = async () => {
-    const { Jimp } = window;
     const newHandle = await window.showSaveFilePicker({ suggestedName: "lightnote.rom" });
     const writableStream = await newHandle.createWritable();
     setProgress(10);
 
-    let files = [];
-    let promises = files.map((file, index) => {
-      return Jimp.read(URL.createObjectURL(file))
-        .then(f => {
-          return f
-            .resize(200, 200)
-            .background(0xFFFFFFFF)
-            .flip(false, true)
-            .getBufferAsync(Jimp.MIME_BMP)
-        })
-        .then(data => {
-          var bmpData = bmpJs.decode(data);
-          const bit1bmp = bit1Encoder.raw(bmpData, 1);
-          let prog = 100 * (index + 1) / files.length;
-          setProgress(prog);
-          console.log('progress: ' + prog);
-          return writableStream.write({
+    writableStream.write({
             type: "write",
-            data: bit1bmp.data,
+            data: imageBuffer,
           })
-        })
         .catch((err) => {
           console.error(err);
         })
-    });
-    console.log(promises);
-    Promise.all(promises)
-      .then(res => {
-        var config_sector = Buffer.alloc(12);
-        var pos = 0;
-        config_sector.writeUInt32LE(0x23571113, pos); pos += 4;
-        config_sector.writeUInt16LE(5000, pos); pos += 2;
-        config_sector.writeUInt32LE(files.length, pos); pos += 4;
-        config_sector.writeUInt8(0x2, pos); pos += 1;
-        config_sector.writeUInt8(0x2, pos); pos += 1;
-        return writableStream.write({
-          type: "write",
-          data: config_sector,
-          position: 16777216 - 0x1000,
-        })
-      })
-      .then(res => {
-        return writableStream.write({
-          type: "write",
-          data: 0,
-          position: 16777216 - 1,
-        })
-      })
       .then(res => { writableStream.close(); });
   };
 
@@ -103,7 +58,7 @@ export default function App() {
       <br />
       <OriginalImage file={file} />
       <div>
-      <ScaledImage file={file} setProgress={setProgress} setScaledImageBuffer={setScaledImageBuffer} />
+      <ScaledImage file={file} setProgress={setProgress} setImageBuffer={setImageBuffer} />
       </div>
     </div>
   );
